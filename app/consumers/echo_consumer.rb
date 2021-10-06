84604cb660ea57aa4e6bedbf96b1c429bbdb31d4
Jean Tessier <jean@jeantessier.com>
2021-10-05 23:38:03 -0700

    Display Kafka message metadata

--- app/consumers/echo_consumer.rb
+++ app/consumers/echo_consumer.rb
@@ -2,6 +2,6 @@ class EchoConsumer < Racecar::Consumer
   subscribes_to ENV['KAFKA_TOPIC'] || 'some-topic'
 
   def process(message)
-    puts "Received message: #{message.value}"
+    puts "#{message.topic}[#{message.partition}] offset: #{message.offset}, timestamp: #{message.create_time}, headers: #{message.headers}, key: #{message.key}, value: #{message.value}"
   end
 end
